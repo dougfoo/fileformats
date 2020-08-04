5616c7d2880826d4ecc38daeb28ec5f384a0968b
dougfoo <dcha@foo-surface3.localdomain>
2020-08-05 00:01:55 +0900

    merged gender, wrote json parq

--- scratch.py
+++ scratch.py
@@ -2,7 +2,25 @@ import pandas as pd
 import sys
 
 df = pd.read_csv("chicago-salary.csv")
+df_mf = pd.read_csv("baby-names.csv")
+df_mf.drop(columns=["year","percent"], inplace=True)
+df_mf.drop_duplicates(inplace=True)
+df_mf["name"] = df_mf["name"].apply(lambda x: x.upper())
+df_mf["sex"].replace({'boy':'M','girl':'F'}, inplace=True)
 
-print(df["Name"].split(",").head())
+df[["Lname","Fname"]] = df["Name"].str.split(',',expand=True)
+df["Fname"] = df["Fname"].str.strip().str.split(" ").str[0]
+df.drop(columns=["Name","Full or Part-Time","Hourly Rate","Typical Hours","Salary or Hourly"], inplace=True)
+df.dropna(subset=["Annual Salary"], inplace=True)
+print(df.head())
+ 
+df_gender = df.merge(df_mf, 'left', left_on='Fname', right_on='name').drop(columns=["name"])
+df_gender["sex"].fillna("?", inplace=True )
 
+print(df_gender.head())
+print(df_gender.size) 
+
+df_gender.to_csv("chicago-salary-gender.csv")
+df_gender.to_json("chicago-salary-gender.json", orient="records" )
+df_gender.to_parquet("chicago-salary-gender.parq")
 
---
+++ test.py
@@ -0,0 +1,5 @@
+print("Hello world")
+
+import sys
+import pandas as pd 
+
